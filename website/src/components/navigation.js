d0e96bb285f4b1619592befafa4a47dac08dfdc8
osc <790990241@qq.com>
2026-07-25 12:45:53 +0800

    Refine documentation navigation

--- website/src/components/navigation.js
+++ website/src/components/navigation.js
@@ -4,16 +4,18 @@ import classNames from 'classnames'
 
 import Link from './link'
 import Dropdown from './dropdown'
+import Icon from './icon'
 import classes from '../styles/navigation.module.sass'
 
 const homeUrl = process.env.NEXT_PUBLIC_BASE_PATH || '/'
+const repositoryUrl = 'https://github.com/di-osc/asr-data'
 
 const NavigationDropdown = ({ items = [], section }) => {
-    const active = items.find(({ text }) => text.toLowerCase() === section)
+    const active = items.find(({ url }) => url === '/asr-data')
     return (
         <Dropdown defaultValue={active?.url || 'title'} className={classes.dropdown}>
             <option value="title" disabled>
-                Libraries
+                文档导航
             </option>
             {items.map(({ text, url }) => (
                 <option key={url} value={url}>
@@ -29,9 +31,7 @@ export default function Navigation({ title, items = [], section, children }) {
         <nav className={classes.root}>
             <Link to={homeUrl} aria-label={title} noLinkLayout>
                 <span className={classes.title}>
-                    <span className={classes['title-name']}>di-osc</span>
-                    <span className={classes['title-divider']} aria-hidden="true" />
-                    <span className={classes['title-product']}>docs</span>
+                    {title}
                 </span>
             </Link>
             <div className={classes.menu}>
@@ -54,6 +54,10 @@ export default function Navigation({ title, items = [], section, children }) {
                     })}
                 </ul>
             </div>
+            <Link to={repositoryUrl} className={classes.github} noLinkLayout>
+                <Icon name="github" width={18} inline />
+                <span>GitHub</span>
+            </Link>
             {children}
         </nav>
     )
